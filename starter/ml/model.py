7c1c53cccfb129496207a2d49e44c5cb377f10d2
bboostdny <bassboostdny@gmail.com>
2024-01-01 14:59:28 +0100

    Completed code for model training

--- starter/ml/model.py
+++ starter/ml/model.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.ensemble import RandomForestClassifier
 
 
 # Optional: implement hyperparameter tuning.
@@ -17,8 +18,10 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
 
-    pass
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -48,7 +51,7 @@ def inference(model, X):
 
     Inputs
     ------
-    model : ???
+    model : sklearn model
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -57,4 +60,5 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+
+    return model.predict(X)
